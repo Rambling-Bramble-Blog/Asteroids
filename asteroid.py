3dc6b06c391c5047448f4aaf1e4e4f7a3e621063
Rambling-Bramble-Blog <rambling.bramble.blog@gmail.com>
2026-01-05 21:03:57 -0800

    Asteroids now split and game is fully playable

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,8 @@
-import pygame
+import pygame, random
+
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS, ASTEROID_MAX_RADIUS
+from logger import log_event
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -14,3 +16,21 @@ class Asteroid(CircleShape):
         # Example of simple rotation and movement logic for the asteroid
         # self.rotation += 20 * dt  # Rotate at 20 degrees per second
         self.position += self.velocity * dt  # Move according to velocity
+        
+    def split(self):
+        self.kill()  # Remove the current asteroid
+        # This method can be called to split the asteroid into smaller pieces
+        if self.radius <= ASTEROID_MIN_RADIUS:  # Do not split if the asteroid is too small
+            return
+        else:
+            log_event("asteroid_split")
+            angle = random.uniform(20, 50)
+            self. new_asteroid1_velocity = self.velocity.rotate(angle)
+            self.new_asteroid2_velocity = self.velocity.rotate(-angle)
+            self.new_asteroid_radius = self.radius - ASTEROID_MIN_RADIUS
+            self.velocity_scale = 1.2
+            
+            self.new_asteroid1 = Asteroid(self.position.x, self.position.y, self.new_asteroid_radius)
+            self.new_asteroid2 = Asteroid(self.position.x, self.position.y, self.new_asteroid_radius)
+            self.new_asteroid1.velocity = self.new_asteroid1_velocity * self.velocity_scale
+            self.new_asteroid2.velocity = self.new_asteroid2_velocity * self.velocity_scale
