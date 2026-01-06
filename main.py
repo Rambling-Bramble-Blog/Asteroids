345c93081d71d16ec91fba84b8cd8ed526d77bf3
Rambling-Bramble-Blog <rambling.bramble.blog@gmail.com>
2026-01-05 20:49:53 -0800

    Asteroids and shots now destroy eachother

--- main.py
+++ main.py
@@ -45,6 +45,13 @@ def main():
                 log_event("player_hit")
                 print("Game over!")
                 sys.exit()
+                
+        for asteroid in asteroids:
+            for shot in shots:
+                if asteroid.collide_with(shot):
+                    log_event("asteroid_shot")
+                    asteroid.kill()
+                    shot.kill()
         
         pygame.display.flip()
         dt = clock.tick (60)/1000  # Limit to 60 frames per second
