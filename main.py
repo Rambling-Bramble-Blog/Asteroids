9974708a83153ed0e9425094d50638de0b03f499
Rambling-Bramble-Blog <rambling.bramble.blog@gmail.com>
2026-01-05 20:18:38 -0800

    Groups added for updatable and  drawable elements

--- main.py
+++ main.py
@@ -8,7 +8,14 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0 # delta time between frames
+    
+    updatable =  pygame.sprite.Group()
+    drawable =  pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     
     while True:
         log_state()
@@ -18,8 +25,12 @@ def main():
                 return
         
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+        
+        updatable.update(dt)
+        
+        for item in drawable:
+            item.draw(screen)
+        
         pygame.display.flip()
         dt = clock.tick (60)/1000  # Limit to 60 frames per second
         # print(dt)
